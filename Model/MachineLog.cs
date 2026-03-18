97d5a6695541b5875aea7801a9ae0f3b3b316a8a
javadocq <han18264@naver.com>
2026-03-18 18:02:11 +0900

    feat: MachineLog Class

---
+++ Model/MachineLog.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_MES_Monitoring_System.Model
+{
+    public class MachineLog
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string MachineName { get; set; }
+        public double Temperature { get; set; }
+        public double Pressure { get; set; }
+        public string Status { get; set; }
+
+        public string LogMessage { get; set; }
+    }
+}
